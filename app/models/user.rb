ba841e1d2b21e74c71db638ed075ef2bd7fff992
Haider Bajwa <haider.bajwa.111@gmail.com>
2021-08-11 00:59:24 +0500

    Added Users SignUp!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+
   has_many :articles
+  has_secure_password
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3 , maximum:32}
   VALID_EMAIL_EXPRESSION = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum:105}, format: {with: VALID_EMAIL_EXPRESSION}
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles , only: [:show, :index, :new, :create, :edit,:update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
